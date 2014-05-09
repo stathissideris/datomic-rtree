4b68ce69244f4eefd5b53dcf283dfb318ee16cf7
tim.terry <tim.terry@agentsmutual.co.uk>
2014-05-09 14:36:10 +0100

    Added distance search exploration code

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.datomic/datomic-free "0.9.4752"]
-                 [quil "1.6.0"]]
+                 [quil "1.6.0"]
+                 [meridian/clj-jts "0.0.2"]]
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.trace "0.7.6"]
                                   [org.clojure/tools.namespace "0.2.4"]]}})
---
+++ test/meridian/datomic_rtree/distance_search.clj
@@ -0,0 +1,59 @@
+(ns meridian.datomic-rtree.distance-search
+  (:use [datomic.api :only (q db) :as d])
+  (:require [clojure.java.io :as io]
+            [meridian.datomic-rtree.rtree :as rtree]
+            [meridian.datomic-rtree.bbox :as bbox]
+            [meridian.datomic-rtree.hilbert :as hilbert]
+            [meridian.datomic-rtree.bulk :as bulk]
+            [meridian.datomic-rtree.shapes :as shapes]
+            [meridian.datomic-rtree.test-utils :as test-utils]
+            [meridian.clj-jts :as jts]))
+
+(comment
+  (defn run-test-query1 [db1]
+    (d/q '[:find ?e :where [?e :node/entry] ] db1)
+    )
+
+  (defn run-test-query2 [db1]
+    (d/q '[:find ?e :where
+           [?e :node/entry]
+           [?e :bbox/max-x ?ex]
+           [?e :bbox/max-y ?ey]
+           [(meridian.datomic-rtree.test-utils/calc-distance ?ex ?ey) ?d]
+           [(< ?d 10)]] db1)
+    ))
+
+(defn create-bbox-for-circle [[center-x center-y] radius]
+  (let [min-x (- center-x radius)
+        min-y (- center-y radius)
+        width (* radius 2)
+        height width]
+    (bbox/bbox min-x min-y width height)))
+
+(defn by-distance [[x y] r]
+  (let [center-point (jts/point [x y])]
+    #(< (.distance (jts/point [(:bbox/max-x %) (:bbox/max-y %)]) center-point))))
+
+(defn distance-search [[center-x center-y] distance db]
+  (let [bbox (create-bbox-for-circle [center-x center-y] distance)
+        _ (println "test 2")
+        results-inside-bbox (rtree/containing (:rtree/root (test-utils/find-tree db)) bbox)]
+    (println "test")
+    (filter (by-distance [center-x center-y] distance) results-inside-bbox)))
+
+(defn print-result [db1 result]
+  (->> result first (d/entity db1) seq clojure.pprint/pprint))
+
+(defn print-all-results [db1 results]
+  (map (partial print-result db1) results))
+
+
+(comment
+  (test-utils/timings 1000 6 3)
+  (def conn (d/connect "datomic:mem://rtrees"))
+  (def db1 (d/db conn))
+  (d/q '[:find ?e :where [?e :node/entry]] db1)
+  (->> (d/q '[:find ?e :where [?e :node/entry]] db1) ffirst (d/entity db1) (seq) (clojure.pprint/pprint))
+  (clojure.pprint/pprint (seq (map #(d/entity db1 %) (first (d/q '[:find ?e :where [?e :node/entry] [?e :bbox/max-y 269.27033000145093 ]] db1)))))
+  (print-all-results db1 (distance-search [1 2] 3 db1))
+  )
