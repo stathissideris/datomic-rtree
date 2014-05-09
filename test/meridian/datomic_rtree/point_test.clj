1302b1d8b07257db7e04d577142ef9bb8db2c7fd
tim.terry <tim.terry@agentsmutual.co.uk>
2014-05-09 16:14:24 +0100

    added unit test

--- test/meridian/datomic_rtree/point_test.clj
+++ test/meridian/datomic_rtree/point_test.clj
@@ -9,7 +9,8 @@
              [bbox :as bbox]
              [bulk :as bulk]
              [shapes :as shapes]
-             [hilbert :as hilbert]]))
+             [hilbert :as hilbert]
+             [distance-search :as dist-search]]))
 
 (deftest point-contained-test
   (are [p bbox] (true? (contained? p bbox))
@@ -41,7 +42,7 @@
                 (point 1 3)
                 (point 3 1)
                 (point 4 4)
-                
+
                 (point 100 100)
                 (point 110 110)
                 (point 90 90)
@@ -50,14 +51,14 @@
                 (point 220 105)
                 (point 220 90)
                 (point 200 90)
-                
+
                 (point 100 350)
                 (point 110 360)
                 (point 90 330)
                 (point 100 300)
                 (point 120 320)
                 (point 100 360)
-                
+
                 (point 550 500)]]
     (do
       (def conn conn);;TODO clean up
@@ -83,6 +84,40 @@
              {:bbox "[200.0 90.0 200.0 90.0]\n", :type :Point}
              {:bbox "[120.0 120.0 120.0 120.0]\n", :type :Point}}
            (search-bbox (bbox/bbox 80 80 230 150)))))))
+
+(deftest test-distance-search
+  (let [uri "datomic:mem://rtrees"
+        conn (utils/create-and-connect-db
+              uri
+              "resources/datomic/schema.edn"
+              "resources/datomic/geojsonschema.edn")
+        hilbert-index-fn (hilbert/index-fn 28 [0.0 600.0])
+        point (partial utils/point-entry hilbert-index-fn)
+        max 6
+        min 3
+        point-count 30
+
+        points [(point 1 1)
+                (point 5 3)
+                (point 1 3)
+                (point 3 1)
+                (point 4 4)]]
+    (do
+      (def conn conn);;TODO clean up
+      (utils/create-tree conn max min)
+      ;;(utils/install-rand-ents conn point-count utils/minimal-entry)
+      @(d/transact conn points)
+      (utils/bulk-load-ents conn max min bulk/dyn-cost-partition))
+    (testing "point insertion to r-tree"
+      (is (=
+           #{{:bbox "[5.0 3.0 5.0 3.0]\n", :type :Point}
+             {:bbox "[1.0 1.0 1.0 1.0]\n", :type :Point}
+             {:bbox "[1.0 3.0 1.0 3.0]\n", :type :Point}
+             {:bbox "[4.0 4.0 4.0 4.0]\n", :type :Point}
+             {:bbox "[3.0 1.0 3.0 1.0]\n", :type :Point}}
+           (dist-search/distance-search [2.0 2.0] 2 (db conn))))
+      )))
+
 
 (comment
   (use '[datomic.api :only (q db) :as d])
